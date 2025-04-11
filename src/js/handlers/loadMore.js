3654f7b5a3bbe76567a32ff4b864832a58fba9b3
Sergiy V <sergiykv@gmail.com>
2025-04-11 23:47:50 +0200

    Homework v3 (LoadMore loader added)

--- src/js/handlers/loadMore.js
+++ src/js/handlers/loadMore.js
@@ -4,24 +4,28 @@ import { showEndMessage, smoothScroll } from '../utils/helpers.js';
 import { buttonService } from '../services/ButtonService.js';
 import { getSearchContext, incrementPage } from './form.js';
 
+const loaderEl = document.querySelector('.load-more-container .loader');
 const loadMoreBtn = document.querySelector('.load-more');
 
 loadMoreBtn.addEventListener('click', async () => {
+  buttonService.hide();
+  loaderEl.classList.remove('is-hidden');
+
   incrementPage();
   const { currentPage, currentQuery, totalPages } = getSearchContext();
-  document.querySelector('.loader').classList.remove('is-hidden');
 
   try {
     const { hits } = await getImages(currentQuery, currentPage);
-    document.querySelector('.loader').classList.add('is-hidden');
+    loaderEl.classList.add('is-hidden');
     renderGallery(hits);
     smoothScroll();
     if (currentPage >= totalPages) {
-      buttonService.hide();
       showEndMessage();
+    } else {
+      buttonService.show();
     }
   } catch (error) {
-    document.querySelector('.loader').classList.add('is-hidden');
+    loaderEl.classList.add('is-hidden');
     console.error(error);
   }
 });
